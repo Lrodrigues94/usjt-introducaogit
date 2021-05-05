13793c7b050a79c2abf706b9c5a170a3cd2b0277
Leonardo Rodrigues <leonardo.rodrigues1994@hotmail.com>
2021-05-05 00:07:26 -0300

    Calculo de salario realizado em funcao do tipo do funcionario

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,23 @@
 public class Empregado{
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
     private String nome;
     private String idade;
+
+    public double calcularSalario(){
+        switch (tipo){
+            case 1: 
+                return salario;
+            case 2:
+                return salario  + salario * comissao;
+            case 3:
+                return salario + bonus;
+            default:
+                return 0;
+        }
+    }
 
     public int getIdade(){
         return idade;
@@ -17,6 +34,4 @@ public class Empregado{
     public void setNome(String nome){
         this.nome = nome;
     }
-    
-    
 }
